7871e2e229da37541640f76052d81bb311b42e23
Sarah Bateman <sarahb3865@gmail.com>
2023-08-06 19:12:34 +1200

    simplified port in app.js to see if it helped back end to connect

--- app.js
+++ app.js
@@ -13,18 +13,19 @@ const app = express();
 connectDB();
 
 // cors
-// app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
 // app.use(express.json({ extended: false }));
+
+const port = 8082;
+
+app.listen(port, () => 
+    console.log(`Server running on port ${port}`));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
-
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
 
 module.exports = app
