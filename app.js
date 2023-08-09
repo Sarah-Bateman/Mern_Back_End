479a660c47d2781b6166125e2cffbf55bc906382
Sarah Bateman <sarahb3865@gmail.com>
2023-08-10 08:46:50 +1200

    Reverted to stock tutorial to try vercel upload again

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
 // app.js
 
 const express = require('express');
-const connectDB = require(path.join('/vercel/output', 'config/db'));
-// const cors = require('cors');
+const connectDB = require('./config/db');
+const cors = require('cors');
 
 // routes
-// const books = require('./routes/api/books');
+const books = require('./routes/api/books');
 
 const app = express();
 
 // Connect Database
-// connectDB();
+connectDB();
 
 // cors
-// app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 // Init Middleware
-// app.use(express.json({ extended: false }));
-
-const port = 8082;
-
-app.listen(port, () => 
-    console.log(`Server running on port ${port}`));
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
-app.get('/cwd', (req, res) => res.send(process.cwd().toString()));
-
 // use Routes
-// app.use('/api/books', books);
+app.use('/api/books', books);
 
-module.exports = app
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
